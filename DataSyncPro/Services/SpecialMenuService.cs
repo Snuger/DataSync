bae263b4bdcff088df8d20e3a6388c53f46c3171
Snuger <explemsnii@outlook.com>
2019-07-07 19:07:44 +0800

    基本菜单的样式调整

--- DataSyncPro/Services/SpecialMenuService.cs
+++ DataSyncPro/Services/SpecialMenuService.cs
@@ -17,8 +17,8 @@ namespace DataSyncPro.Model
         {
             List<BasicMenu> basicMenus = new List<BasicMenu>() {
                  new BasicMenu(){Id="1", Name="系统设置", ParentId="0", Kiind=PackIconKind.Settings,Content=new WellcomPanel()},
-                  new BasicMenu(){Id="2", Name="数据源设置", ParentId="0", Kiind=PackIconKind.Database,Content=new DataSourceSettinsPanel()},
-                  new BasicMenu(){Id="4", Name="目标数据源", ParentId="0", Kiind=PackIconKind.TargetArrow,Content=new TargetDataBaeSettingsPanel()},
+                  new BasicMenu(){Id="2", Name="数据源设置", ParentId="0", Kiind=PackIconKind.DatabaseExport,Content=new DataSourceSettinsPanel()},
+                  new BasicMenu(){Id="4", Name="目标数据源", ParentId="0", Kiind=PackIconKind.DatabaseImport,Content=new TargetDataBaeSettingsPanel()},
                   new BasicMenu(){Id="3", Name="数据同步", ParentId="0", Kiind=PackIconKind.UploadNetwork, Content=new AutomaticUploadPnel()},
                   new BasicMenu(){Id="5", Name="用户设置", ParentId="0", Kiind=PackIconKind.SettingsBox},
                   new BasicMenu(){Id="6", Name="用户设置", ParentId="0", Kiind=PackIconKind.RouterWirelessSettings},
